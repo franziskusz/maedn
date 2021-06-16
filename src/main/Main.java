4da9a0927e18e30168d912c5f26dec76836459d9
toniromer <toniromer@tonis-mbp.fritz.box>
2021-06-16 09:23:17 +0200

    class_suggestions

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,9 +3,13 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-    	//test eclipse
-        //test intelliJ
-    	//testkommentar
-    	//testkommentar_toni
+    	//Klassenvorschläge:
+    	
+    	/*
+    	 * Klasse dice mit Zufallszahl 1-6
+    	 * Klasse mit Spielfigur bsp (public Figur mit String name und int spielerID)
+    	 * Klasse Spielfeld
+    	 * Klasse Spieler (Funktionen würfeln, erfragen der zu bewegenden Spielfigur)
+    	 */
     }
 }
