381f34d7866ad886e80661af3a3662a5a9f887c9
Franziskus Zillmann <Frungz@gravemind.fritz.box>
2021-06-19 16:09:12 +0200

    Graphen Package und auskommentierte Testausgabe in der Main (nullpointer ex)

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,20 @@
 package main;
 
+import main.graph.Graph;
+
 public class Main {
 
 	public static void main(String[] args) {
 
 		GUI gui = new GUI();
 		
+		
+		
+		// Testausgabe Graph resultiert in Nullpointer Exception beim Anlegen der Kanten
+		/*
+		Graph board = new Graph();
+		board.initGraph();
+		board.testPrint();
+		*/
 	}
 }
