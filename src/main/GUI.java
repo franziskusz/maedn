93b8bdeb10a6062a3df8ca6fbd7ee650daae44f6
JohannesGGE <johannesgge@gmail.com>
2021-06-19 16:03:55 +0200

    GUI

--- src/main/GUI.java
+++ src/main/GUI.java
@@ -6,25 +6,23 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 public class GUI extends JFrame{
 	
 	private Controller controller;
 
-	private ImageIcon background;
 	private JLabel myLabel;
 	private JButton startButton;
-	private JPanel content;
+	private Panel_with_background content;
 	
 	public GUI(){
-		controller= new Controller(this);	
+		controller= new Controller(this);
 		
-		Panel_with_background content = new Panel_with_background(new ImageIcon("/pruefung_aop/background.png").getImage());
+		content = new Panel_with_background(new ImageIcon("./background.png").getImage());
 		
 		setTitle("Mensch ärgere dich nicht");
-		add(myLabel);
-		setSize(700,700); 
+		setSize(700,700);
+		setResizable(true);
 	
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -32,15 +30,8 @@ public class GUI extends JFrame{
 		setContentPane(content);
 		pack();
 		setVisible(true);
-		
-		
 	}
-	
-	
-	
-	
-	
-	
+
 
 }
 
