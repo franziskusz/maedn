a797ad4d6900eeddd9cb33536620a1f18e01f044
toniromer <toniromer@tonis-mbp.fritz.box>
2021-06-19 19:14:38 +0200

    new

--- src/main/Circle.java
+++ src/main/Circle.java
@@ -2,14 +2,18 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Image;
+import javax.swing.ImageIcon;
 
 import javax.swing.*;
 
 
 
-public class Circle extends JPanel {
+public class Circle extends JFrame {
+	
+	/*ImageIcon Image = new ImageIcon("circle.png");
 	
 	
 	private Image img;
@@ -20,7 +24,7 @@ public class Circle extends JPanel {
 
 	public Circle(Image img) {
 		this.img = img;
-		Dimension size = new Dimension((10),(10));
+		Dimension size = new Dimension((100),(100));
 		setPreferredSize(size);
 		setMinimumSize(size);
 		setMaximumSize(size);
@@ -31,5 +35,26 @@ public class Circle extends JPanel {
 	public void paintComponent(Graphics g) {
 		g.drawImage(img, 0, 0, null);
 	}
+*/
+	private ImageIcon image1;
+	private JLabel label1;
+	
+	
+	void ImageTutorial(){
+		setLayout(new FlowLayout());
+		
+		image1= new ImageIcon(getClass().getResource("./circle.png"));
+		label1 =new JLabel(image1);
+		add(label1);
+	}
+	
+		
+		
+		
+		
+	
+	
+	
+	
 
 }
