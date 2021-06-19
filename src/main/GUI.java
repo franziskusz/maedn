7e8a3f0a1b012834c8c17075d312a8159fa2f01c
toniromer <toniromer@tonis-mbp.fritz.box>
2021-06-19 15:52:18 +0200

    tonis work

--- src/main/GUI.java
+++ src/main/GUI.java
@@ -1,11 +1,39 @@
 package main;
 
-public class GUI {
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class GUI extends JFrame{
 	
 	private Controller controller;
 
+	private ImageIcon background;
+	private JLabel myLabel;
+	private JButton startButton;
+	private JPanel content;
+	
 	public GUI(){
 		controller= new Controller(this);	
+		
+		Panel_with_background content = new Panel_with_background(new ImageIcon("/pruefung_aop/background.png").getImage());
+		
+		setTitle("Mensch ärgere dich nicht");
+		add(myLabel);
+		setSize(700,700); 
+	
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+
+		setContentPane(content);
+		pack();
+		setVisible(true);
+		
+		
 	}
 	
 	
@@ -15,3 +43,19 @@ public class GUI {
 	
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/Panel_with_background.java
@@ -0,0 +1,28 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Panel_with_background extends JFrame {
+
+
+private Image img;
+
+public Panel_with_background(String img) {
+this(new ImageIcon(img).getImage());
+}
+
+public Panel_with_background(Image img) {
+this.img = img;
+Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
+setPreferredSize(size);
+setMinimumSize(size);
+setMaximumSize(size);
+setSize(size);
+setLayout(null);
+}
+
+public void paintComponent(Graphics g) {
+g.drawImage(img, 0, 0, null);
+}
+}
