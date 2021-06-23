9781100d300ddd1f7c0a7851082ed80181953842
toniromer <toniromer@tonis-mbp.fritz.box>
2021-06-23 15:43:39 +0200

    Buttons and stuff

--- src/main/gui/GameGUI.java
+++ src/main/gui/GameGUI.java
@@ -6,6 +6,7 @@ import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -14,6 +15,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 
 
@@ -22,8 +24,6 @@ import javax.swing.JPanel;
  * 	Hintergrund+Spielfeld nicht möglich Warum?
  *
  */
-
-
 
 public class GameGUI extends JFrame{
 
@@ -39,6 +39,21 @@ public class GameGUI extends JFrame{
 	private JPanel rightPanel;
 	private Panel_with_background content;
 	private Circle circle;
+	
+	public static final String WUERFELN = "WUERFELN";
+	public static final String OPTION_1 = "OPTION_1";
+	public static final String OPTION_2 = "OPTION_2";
+	public static final String OPTION_3 = "OPTION_3";
+	public static final String OPTION_4 = "OPTION_4";
+
+	private JButton btnWuerfel = new JButton();
+	private JButton btnOption1 = new JButton();
+	private JButton btnOption2 = new JButton();
+	private JButton btnOption3 = new JButton();
+	private JButton btnOption4 = new JButton();
+
+	private JTextField text = new JTextField();
+	private JTextField wZahl = new JTextField();
 	 
 	public GameGUI(){
 		
@@ -51,33 +66,135 @@ public class GameGUI extends JFrame{
 		panel.setPreferredSize(new Dimension(700, 700));
 		this.add(panel, BorderLayout.CENTER);
 */
+		
+		/*
+		setLayout(new FlowLayout());
+
+		text.setEditable(false);
+		text.setText("?");
+		text.setPreferredSize( new Dimension( 200, 24 ) );
+		wZahl.setSize(400, 10);
+		add(text);
+
+		wZahl.setEditable(false);
+		wZahl.setText("0");
+		wZahl.setSize(10, 10);
+		add(wZahl);
+
+		btnWuerfel.setText("Würfeln");
+		btnWuerfel.setActionCommand(WUERFELN);
+		add(btnWuerfel);
+
+		btnOption1.setText("Option 1");
+		btnOption1.setActionCommand(OPTION_1);
+		add(btnOption1);
+
+		btnOption2.setText("Option 2");
+		btnOption2.setActionCommand(OPTION_2);
+		add(btnOption2);
+
+		btnOption3.setText("Option 3");
+		btnOption3.setActionCommand(OPTION_3);
+		add(btnOption3);
+
+		btnOption4.setText("Option 3");
+		btnOption4.setActionCommand(OPTION_3);
+		add(btnOption4);
+
+
+		pack();
+		
+		*/
+		
 		JLayeredPane layeredPane = new Panel_with_background(new ImageIcon("./images/background.png").getImage());
-		layeredPane.setPreferredSize(new Dimension(700, 800));
+		layeredPane.setPreferredSize(new Dimension(580, 580));
 		this.add(layeredPane,BorderLayout.CENTER);
 		
 		
 		
 		JPanel panel1 = new JPanel();
-		panel1.setBackground(Color.red);
-		panel1.setPreferredSize(new Dimension(700, 50));
+		//panel1.setBackground(Color.red);
+		panel1.setPreferredSize(new Dimension(580, 50));
 		this.add(panel1, BorderLayout.NORTH);
 		
 		JPanel panel2 = new JPanel();
-		panel2.setBackground(Color.red);
-		panel2.setPreferredSize(new Dimension(700, 50));
+		//panel2.setBackground(Color.red);
+		panel2.setPreferredSize(new Dimension(580, 50));
 		this.add(panel2, BorderLayout.SOUTH);
+		
+		
+		
 
+		wZahl.setEditable(false);
+		wZahl.setText("0");
+		wZahl.setSize(10, 10);
+		panel1.add(wZahl);
 
+		btnWuerfel.setText("Würfeln");
+		btnWuerfel.setActionCommand(WUERFELN);
+		panel2.add(btnWuerfel);
+
+		btnOption1.setText("Option 1");
+		btnOption1.setActionCommand(OPTION_1);
+		panel2.add(btnOption1);
+
+		btnOption2.setText("Option 2");
+		btnOption2.setActionCommand(OPTION_2);
+		panel2.add(btnOption2);
+
+		btnOption3.setText("Option 3");
+		btnOption3.setActionCommand(OPTION_3);
+		panel2.add(btnOption3);
+
+		btnOption4.setText("Option 4");
+		btnOption4.setActionCommand(OPTION_3);
+		panel2.add(btnOption4);
+		
+		
+		
+	
+		
+		
+	
+	
+	
+		
+		
 
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.pack();
 		this.setVisible(true);
 		this.setResizable(false);
 
+		
+		
 
 	}	
 	
+	public JButton getBtnWuerfel() {
+		return btnWuerfel;
+	}
 
+	public JButton getBtnOption1() {
+		return btnOption1;
+	}
+
+	public JButton getBtnOption2() {
+		return btnOption2;
+	}
+
+	public JButton getBtnOption3() {
+		return btnOption3;
+	}
+
+	public JButton getBtnOption4() {
+		return btnOption4;
+	}
+
+	public JTextField getText() {
+		return text;
+	}
+	
 }
 
 
--- src/main/gui/Panel_with_background.java
+++ src/main/gui/Panel_with_background.java
@@ -27,117 +27,120 @@ public class Panel_with_background extends JLayeredPane {
 		 // 					(Zeile,Spalte)
 		 
 		 //Spielfeld
-		 g.setColor(Color.black);
-		 g.fillOval(200,40,30,30);//0,4 überarbeiten!
-		 g.fillOval(240,40,30,30);//0,6
-		 g.fillOval(40,240,30,30);//5,0
-		 g.fillOval(40,280,30,30);//6,0
-		 g.fillOval(440,200,30,30);//4,10
-		 g.fillOval(440,240,30,30);//5,10
-		 g.fillOval(240,440,30,30);//10,5
-		 g.fillOval(280,440,30,30);//10,6
-		 
-		 
-		 g.fillOval(80,200,30,30);//4,1
-		 g.fillOval(120,200,30,30);//4,2
-		 g.fillOval(160,200,30,30);//4,3
-		 g.fillOval(200,200,30,30);//4,4
-		 g.fillOval(280,200,30,30);//4,6
-		 g.fillOval(320,200,30,30);//4,7
-		 g.fillOval(360,200,30,30);//4,8
-		 g.fillOval(400,200,30,30);//4,9
+				 g.setColor(Color.black);
+
+				 g.fillOval(220,20,40,40);//0,4
+				 g.fillOval(270,20,40,40);//0,5
+
+		 		 g.fillOval(20,270,40,40);//5,0
+		 		 g.fillOval(20,320,40,40);//6,0
+		 		 g.fillOval(520,220,40,40);//4,10
+		 		 g.fillOval(520,270,40,40);//5,10
+		 		 g.fillOval(270,520,40,40);//10,5
+		 		 g.fillOval(320,520,40,40);//10,6
+
+
+		 		 g.fillOval(70,220,40,40);//4,1
+		 		 g.fillOval(120,220,40,40);//4,2
+		 		 g.fillOval(170,220,40,40);//4,3
+		 		 g.fillOval(220,220,40,40);//4,4
+		 		 g.fillOval(320,220,40,40);//4,6
+		 		 g.fillOval(370,220,40,40);//4,7
+		 		 g.fillOval(420,220,40,40);//4,8
+		 		 g.fillOval(470,220,40,40);//4,9
+			 	
+		 		 
+		 		 g.fillOval(70,320,40,40);//6,1
+		 		 g.fillOval(120,320,40,40);//6,2
+		 		 g.fillOval(170,320,40,40);//6,3
+		 		 g.fillOval(220,320,40,40);//6,4
+
+		 		
+		 		 g.fillOval(320,320,40,40);//6,6
+		 		 g.fillOval(370,320,40,40);//6,7
+		 		 g.fillOval(420,320,40,40);//6,8
+		 		 g.fillOval(470,320,40,40);//6,9
+		 		 
+
+		 		 g.fillOval(220,70,40,40);//1,4
+		 		 g.fillOval(220,120,40,40);//2,4
+		 		 g.fillOval(220,170,40,40);//3,4
+
+		 		 
+		 		 g.fillOval(320,70,40,40); //1,6
+		 		 g.fillOval(320,120,40,40); //2,6
+		 		 g.fillOval(320,170,40,40); //3,6
+
+		 		
+		 		 g.fillOval(220,470,40,40); //9,4
+		 		 g.fillOval(220,420,40,40);	//8,4
+		 		 g.fillOval(220,370,40,40); //7,4
+
+		 	
+		 		 g.fillOval(320,470,40,40); //9,6
+		 		 g.fillOval(320,420,40,40);	//8,6
+		 		 g.fillOval(320,370,40,40); //7,6
+
+
+		 		 g.setColor(Color.blue);
+		 		 //Haus 
+		 		 g.fillOval(20,20,40,40); //0,0
+		 		 g.fillOval(70,20,40,40); //0,1
+		 		 g.fillOval(20,70,40,40); //1,0
+		 		 g.fillOval(70,70,40,40); //1,1
+		 		 //Hotzone
+		 		 g.fillOval(20,220,40,40); //4,0
+		 		 //Safezone
+		 		 g.fillOval(70,270,40,40); //5,1
+		 		 g.fillOval(120,270,40,40); //5,2
+		 		 g.fillOval(170,270,40,40); //5,3
+		 		 g.fillOval(220,270,40,40); //5,4
+
+		 		 
+		 		 g.setColor(Color.green);
+		 		 //Haus
+		 		 g.fillOval(470,20,40,40);//0,9
+		 		 g.fillOval(470,70,40,40);//1,9
+		 		 g.fillOval(520,20,40,40);//0,10
+		 		 g.fillOval(520,70,40,40);//1,10
+		 		 //Hotzone
+		 		 g.fillOval(320,20,40,40);//0,6
+		 		 //Safezone
+		 		 g.fillOval(270,70,40,40); //1,5
+		 		 g.fillOval(270,120,40,40); //2,5
+		 		 g.fillOval(270,170,40,40); //3,5
+		 		 g.fillOval(270,220,40,40); //4,5
 		 
 
-		 g.fillOval(80,280,30,30);//6,1
-		 g.fillOval(120,280,30,30);//6,2
-		 g.fillOval(160,280,30,30);//6,3
-		 g.fillOval(200,280,30,30);//6,4
-		
-		 g.fillOval(280,280,30,30);//6,6
-		 g.fillOval(320,280,30,30);//6,7
-		 g.fillOval(360,280,30,30);//6,8
-		 g.fillOval(400,280,30,30);//6,9
+		 		 g.setColor(Color.red);
+		 		 //Haus
+		 		 g.fillOval(20,470,40,40);//9,0
+		 		 g.fillOval(20,520,40,40);//10,0
+		 		 g.fillOval(70,470,40,40);//9,1
+		 		 g.fillOval(70,520,40,40);//10,1
+		 		 //Hotzone
+		 		 g.fillOval(220,520,40,40);//10,4
+		 		 //Safezone
+		 		 g.fillOval(270,470,40,40); //9,5
+		 		 g.fillOval(270,420,40,40);	//8,5
+		 		 g.fillOval(270,370,40,40); //7,5
+		 		 g.fillOval(270,320,40,40);	//6,5
+
+		 		 
+		 		 g.setColor(Color.yellow);
+		 		 //Haus
+		 		 g.fillOval(470,470,40,40); //9,9
+		 		 g.fillOval(470,520,40,40); //10,9
+		 		 g.fillOval(520,470,40,40); //9,10
+		 		 g.fillOval(520,520,40,40); //10,10
+		 		 //Hotzone
+		 		 g.fillOval(520,320,40,40);//6,10
+		 		 //Safezone
+		 		 g.fillOval(320,270,40,40);//5,6
+		 		 g.fillOval(370,270,40,40);//5,7
+		 		 g.fillOval(420,270,40,40);//5,8
+		 		 g.fillOval(470,270,40,40);//5,9
 		 
-		
-		 
-		 g.fillOval(200,80,30,30);//1,4
-		 g.fillOval(200,120,30,30);//2,4
-		 g.fillOval(200,160,30,30);//3,4
-		 
-		 
-		 g.fillOval(280,80,30,30); //1,6
-		 g.fillOval(280,120,30,30); //2,6
-		 g.fillOval(280,160,30,30); //3,6
-		
-		
-		 g.fillOval(200,400,30,30); //9,4
-		 g.fillOval(200,360,30,30);	//8,4
-		 g.fillOval(200,320,30,30); //7,4
-		
-		 g.fillOval(280,400,30,30); //9,6
-		 g.fillOval(280,360,30,30);	//8,6
-		 g.fillOval(280,320,30,30); //7,6
-		 
-		 
-		 g.setColor(Color.blue);
-		 //Haus 
-		 g.fillOval(40,40,30,30); //0,0
-		 g.fillOval(80,40,30,30); //0,1
-		 g.fillOval(40,80,30,30); //1,0
-		 g.fillOval(80,80,30,30); //1,1
-		 //Hotzone
-		 g.fillOval(40,200,30,30); //4,0
-		 //Safezone
-		 g.fillOval(80,240,30,30); //5,1
-		 g.fillOval(120,240,30,30); //5,2
-		 g.fillOval(160,240,30,30); //5,3
-		 g.fillOval(200,240,30,30); //5,4
-		 
-	 
-		 g.setColor(Color.green);
-		 //Haus
-		 g.fillOval(400,40,30,30);//0,9
-		 g.fillOval(400,80,30,30);//1,9
-		 g.fillOval(440,40,30,30);//0,10
-		 g.fillOval(440,80,30,30);//1,10
-		 //Hotzone
-		 g.fillOval(280,40,30,30);//0,7
-		 //Safezone
-		 g.fillOval(240,80,30,30); //1,5
-		 g.fillOval(240,120,30,30); //2,5
-		 g.fillOval(240,160,30,30); //3,5
-		 g.fillOval(240,200,30,30); //4,5
-		 
-		 
-		 g.setColor(Color.red);
-		 //Haus
-		 g.fillOval(40,400,30,30);//9,0
-		 g.fillOval(40,440,30,30);//10,0
-		 g.fillOval(80,400,30,30);//9,1
-		 g.fillOval(80,440,30,30);//10,1
-		 //Hotzone
-		 g.fillOval(200,440,30,30);//10,4
-		 //Safezone
-		 g.fillOval(240,400,30,30); //9,5
-		 g.fillOval(240,360,30,30);	//8,5
-		 g.fillOval(240,320,30,30); //7,5
-		 g.fillOval(240,280,30,30);	//6,5
-		 
-		 
-		 g.setColor(Color.yellow);
-		 //Haus
-		 g.fillOval(400,400,30,30); //9,9
-		 g.fillOval(400,440,30,30); //10,9
-		 g.fillOval(440,400,30,30); //9,10
-		 g.fillOval(440,440,30,30); //10,10
-		 //Hotzone
-		 g.fillOval(440,280,30,30);//6,10
-		 //Safezone
-		 g.fillOval(280,240,30,30);//5,6
-		 g.fillOval(320,240,30,30);//5,7
-		 g.fillOval(360,240,30,30);//5,8
-		 g.fillOval(400,240,30,30);//5,9
-		 
-		 
+ 
 	 }
 }
