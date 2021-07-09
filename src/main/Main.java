b9b04f2b28628c6eafd7929c6a8f28b9b0c89a78
JohannesGGE <johannesgge@gmail.com>
2021-07-09 23:50:07 +0200

    SetupGUI + Bots + GUI Methodennamen anpassung

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,34 +1,10 @@
 package main;
 
-import main.controller.GameController;
-import main.gui.GameGUI;
 import main.gui.SetupGUI;
-import main.model.GameModel;
-import main.model.enums.PlayerColor;
-import main.model.graph.Graph;
-import main.model.player.Gamer;
-import main.model.player.Player;
-
-import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		
-		ArrayList<Player> INITIAL_PLAYERS = new ArrayList<>();
-		INITIAL_PLAYERS.add(new Gamer(PlayerColor.RED));
-		INITIAL_PLAYERS.add(new Gamer(PlayerColor.BLUE));
-		INITIAL_PLAYERS.add(new Gamer(PlayerColor.GREEN));
-		INITIAL_PLAYERS.add(new Gamer(PlayerColor.YELLOW));
-
-		
-		GameModel gameModel = new GameModel(INITIAL_PLAYERS);
-		GameGUI gameGui = new GameGUI(gameModel.getPieces());
-		GameController gameController = new GameController(gameModel, gameGui);
-		
-		/*
 		SetupGUI setupGUI = new SetupGUI();
-		*/
 	}
 }
--- src/main/SleepThread.java
+++ src/main/SleepThread.java
@@ -1,21 +1,33 @@
 package main;
 
 import main.gui.GameGUI;
+import main.model.GameModel;
+import main.model.enums.BotAction;
 
 public class SleepThread implements Runnable {
 
-	GameGUI gameGUI;
+	GameModel gameModel;
+	BotAction botAction;
 
-	public SleepThread(GameGUI gameGUI) {
-		this.gameGUI = gameGUI;
+	public SleepThread(GameModel gameModel, BotAction botAction) {
+		this.gameModel = gameModel;
+		this.botAction = botAction;
 	}
 
 	@Override
 	public void run() {
 		try {
-			// thread to sleep for 1000 milliseconds
-			Thread.sleep(2000);
-			gameGUI.getText().setText("???");
+			Thread.sleep(1000);
+
+			switch(botAction) {
+				case DICE:
+					gameModel.diceRoll();
+					break;
+				case PERFORM_OPTION:
+					gameModel.performOption(gameModel.getOptions().get(gameModel.getRandom().nextInt(gameModel.getOptions().size())));
+					break;
+			}
+
 		} catch(Exception e) {
 			System.out.println(e);
 		}
