ab51fbf21ba9ed0086d65671b39fc9c249743467
toniromer <toniromer@tonis-mbp.fritz.box>
2021-07-20 21:58:36 +0200

    Ausgabefelder optik

--- src/main/gui/RoundJTextField.java
+++ src/main/gui/RoundJTextField.java
@@ -1,6 +1,9 @@
 package main.gui;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.Shape;
 import java.awt.geom.RoundRectangle2D;
 
@@ -13,9 +16,14 @@ public class RoundJTextField extends JTextField {
         setOpaque(false); // As suggested by @AVD in comment.
     }
     protected void paintComponent(Graphics g) {
+    	 Graphics2D g2d = (Graphics2D) g;
          g.setColor(getBackground());
          g.fillRoundRect(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+         g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY); 
          super.paintComponent(g);
+         
+     
     }
     protected void paintBorder(Graphics g) {
          g.setColor(getForeground());
